d9ea4e5f3d1cd18f80e1c3334ae7aa4f492af392
c4rt0 <c4rt0gr4ph3r@gmail.com>
2024-06-11 09:34:41 +0100

    Port automatic ami recognition
    ref: https://github.com/coreos/coreos-tutorials/commit/97376716dce47080dd9e8f016753c55c5c79da96

--- fedora-coreos/provisioning/main.tf
+++ fedora-coreos/provisioning/main.tf
@@ -8,13 +8,15 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
+    http = {
+      source  = "hashicorp/http"
+      version = "2.1.0"
+    }
   }
 }
 
-provider "aws" {
-  region = "eu-central-1"
-}
 provider "ct" {}
+provider "http" {}
 
 variable "student_password_hash" {
  type        = string
@@ -24,6 +26,8 @@ variable "core_user_ssh_pubkey_string" {
  type        = string
 }
 
+data "aws_region" "aws_region" {}
+
 data "ct_config" "butane" {
   content = templatefile("fcos-lab-tutorial.bu", {
     student_password_hash = bcrypt(var.student_password_hash)
@@ -32,11 +36,24 @@ data "ct_config" "butane" {
   strict = true
 }
 
+# Gather information about the AWS image for the current region
+data "http" "stream_metadata" {
+  url = "https://builds.coreos.fedoraproject.org/streams/stable.json"
+
+  request_headers = {
+    Accept = "application/json"
+  }
+}
+# Lookup the x86 AWS image for the current AWS region
+locals {
+  ami = lookup(jsondecode(data.http.stream_metadata.body).architectures.x86_64.images.aws.regions, data.aws_region.aws_region.name).image
+}
+
 resource "aws_instance" "fcos-lab-instance" {
   tags = {
     Name = "fcos-lab"
   }
-  ami           = "ami-06128ecf4b4101217"
+  ami           = local.ami
   instance_type = "c5n.metal"
   user_data     = data.ct_config.butane.rendered
   associate_public_ip_address = "true"
